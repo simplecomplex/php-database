002ddd594c6a04a5d93a18aee183d411aa2fa30e
jacobfriis <jacob.friis@simplecomplex.net>
2018-05-21 14:14:54 +0200

    Prologue typo

--- tests/src/MsSql/ResultResetTest.php
+++ tests/src/MsSql/ResultResetTest.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * SimpleComplex PHP Database
- * @link      https://github.com/simplecomplex/php-databse
+ * @link      https://github.com/simplecomplex/php-database
  * @copyright Copyright (c) 2018 Jacob Friis Mathiasen
- * @license   https://github.com/simplecomplex/php-databse/blob/master/LICENSE (MIT License)
+ * @license   https://github.com/simplecomplex/php-database/blob/master/LICENSE (MIT License)
  */
 declare(strict_types=1);
 
